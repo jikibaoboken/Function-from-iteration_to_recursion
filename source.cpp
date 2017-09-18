38b31ae2dd15b3610769257f1f4e434f848b0a80
刘瑞琦 <15764949052@163.com>
2017-09-18 20:31:25 +0800

    Add files via upload

---
+++ source.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void display(vector<int> p)
+{
+	for (int i = 0; i < p.size();i++)
+	{
+		cout << p.at(i);
+	}
+	cout << endl;
+}
+int f(int n, int  k,int start,vector<int>p)
+{
+	for (int i=start; i<n-k+1; i++)
+	{
+		if (0 == k)
+		{
+			display(p);
+			return 0;
+		}
+		else
+		{
+			p.push_back(i);
+			f(n, k - 1,i+1,p);
+			p.pop_back();
+		}
+	}
+}
+/*
+vector<int>  f2(int n, int toPick, vector<int> arr)
+{
+	if (0 == toPick)
+	{
+		display(arr);
+	}
+}*/
+
+int main()
+{
+	vector<int> ans,p;
+	vector<int> test = { 1,2,3,4,5 };
+	display(test);
+	f(7, 4,0,p);
+	system("pause");
+	return 0;
+}
